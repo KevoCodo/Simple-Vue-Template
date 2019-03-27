7397cf49341dd1e48815c937b29786d57f608d0b
Kevin Gay <kevin.gay1@gmail.com>
2019-03-26 21:50:07 -0400

    A Hollow Vue Template

---
+++ vue-practice/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <h1>Welcome {{msg}}</h1>
+        <button v-on:click='greeting'>Click Here</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return{
+           title: 'Welcome to Vue Js Practice' 
+        }
+    },
+    props:{
+      
+    },
+    methods:{
+        greeting(){
+            alert('clicked');
+        }
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
+<!--What is code splitting
+    Transpulation
+    Salesforce
+    BlueOcean
+    Legacy
+    Evestment - Networking opportunity
+    Micro frontends
+    Monolithic Frontends
+    hash - allows for files to be cached for re-render
+    webpack-manifest-plugin
+    devtools network filters - alerts
+    helps reduce already loaded files
+-->
